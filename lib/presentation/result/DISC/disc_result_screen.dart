2ff577b765b43f15316381654034545bba186a68
Omar_Ahmed <omarahmed1998991@gmail.com>
2023-04-13 13:43:41 +0200

    Disc with grid view

--- lib/presentation/result/DISC/disc_result_screen.dart
+++ lib/presentation/result/DISC/disc_result_screen.dart
@@ -2,14 +2,13 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:mutqen/presentation/result/Fare2/resultdetails_screen.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:mutqen/resources/color_manager.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
-
 import '../../../data/model/result.dart';
-import '../../../resources/assets_manager.dart';
 import '../../../resources/common_widgets/app_bar.dart';
 import '../../../resources/strings_manager.dart';
+import '../Fare2/resultdetails_screen.dart';
+import 'DISC_details.dart';
 
 
 class disc_result_page extends StatefulWidget {
@@ -27,81 +26,79 @@ class _disc_result_page extends State<disc_result_page> {
       child: Scaffold(
         appBar: getAppBarWidgetWithNotificationIcon("نتيجه DISC", context),
         body: Container(
-          padding: EdgeInsets.only(top: 20),
-          child: ListView.separated(
-            padding:  EdgeInsets.only(bottom: 20),
-            itemCount: widget.results.length,
+          padding: const EdgeInsets.only(top: 20),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // Number of columns in the grid
+              childAspectRatio: 0.7, // Ratio of child item's height to width
+            ),
+            padding:  const EdgeInsets.only(bottom: 20),
+            itemCount: 4,
             itemBuilder: (BuildContext context, int index) {
-              return UnconstrainedBox(
-                child: InkWell(
+              return
+                InkWell(
                   onTap: (){
                     PersistentNavBarNavigator.pushNewScreen(context, screen: resultdetails_page(widget.results[index]));
                   },
-                  child: Container(
-                    padding: EdgeInsets.all(9.0),
-                    height: 110.h,
-                    width: 300.w,
-                    decoration: BoxDecoration(boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.2),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                        color: Colors.white70,
-                        borderRadius: BorderRadius.circular(15)),
+
+                  child: Card(
+                    elevation: 1.5,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        SizedBox(height: 8,),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Column(
-                              children: [
-                                Text(widget.results[index].title,style: TextStyle(
-                                    color: widget.results[index].color
-                                ),),
-                                SizedBox(height: 10,),
-                                Text(widget.results[index].description ,style: TextStyle(color: Colors.black45),),
-                              ],
+                        // Card's title
+                        Container(
+                          height: 110.h,
+                          decoration: BoxDecoration(
+                            borderRadius:
+                            const BorderRadius.all(Radius.circular(10)),
+                            //color: widget.results[index].color,
+                            gradient: LinearGradient(colors: [
+                              widget.results[index].color,
+                              widget.results[index].color.withOpacity(0.9),
+                              widget.results[index].color.withOpacity(0.8),
+
+                            ])
+                          ),
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Text(widget.results[index].title,style: TextStyle(
+                                  color:Colors.white,fontSize: 45.sp,
+                              ),)
                             ),
-                            SizedBox(width: 10,),
-                            Container(
-                              child: CircularPercentIndicator(
-                                radius: 40.0,
-                                lineWidth: 10.0,
-                                animation: true,
-                                animationDuration: 2000,
-                                percent: widget.results[index].res/100,
-                                center: new Text(
-                                  widget.results[index].res.toString()+"%",
-                                  style:
-                                  new TextStyle(fontWeight: FontWeight.bold, fontSize: 13.0),
-                                ),
-                                linearGradient: LinearGradient(colors:
-                                [widget.results[index].color.withOpacity(0.1),widget.results[index].color.withOpacity(0.5),widget.results[index].color]),
-                                circularStrokeCap: CircularStrokeCap.round,
-                                // progressColor: widget.results[index].color,
-                              ),
-                            ),
-                          ],
+                          ),
                         ),
-                        SizedBox(height: 8,),
+                        // Card's description
+                        Container(
+                          height: 70.h,
+                          child: Padding(
+                            padding: const EdgeInsets.all(12),
+                            child: Text(widget.results[index].description ,style: TextStyle(color: Colors.black54,fontSize: 15.sp),),
+                          ),
+                        ),
+                        Container(
+                          height: 50.h,
+                            alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child:
+                            Text(
+                              "${widget.results[index].res}%",
+                              style:
+                               TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0.sp),
+                            ),),
+                        ),
                       ],
                     ),
                   ),
-                ),
-              );
+                );
             },
-            separatorBuilder: (BuildContext context, int index) {
-              return SizedBox(
-                height: 18,
-              );
-            },),
+           ),
         ) ,
       ),
     );
   }
 }
+
+
